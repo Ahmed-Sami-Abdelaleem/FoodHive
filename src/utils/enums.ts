b326c785b6bd698e242ae3bffcff45f8fc4356e4
Ahmed-Sami-Abdelaleem <ahmed222333sami@gmail.com>
2025-01-04 11:55:53 +0200

    enums needed for now

---
+++ src/utils/enums.ts
@@ -0,0 +1,25 @@
+export enum HttpMethod {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+}
+export enum Environment {
+  DEVELOPMENT = "development",
+  PRODUCTION = "production",
+  TEST = "test",
+}
+export enum UserRole {
+  USER = "USER",
+  ADMIN = "ADMIN",
+}
+export enum Routes {
+  BASE = "http://localhost:3000",
+  HOME = "/",
+  LOGIN = "/login",
+  REGISTER = "/register",
+  DASHBOARD = "/dashboard",
+  PROFILE = "/profile",
+  USERS = "/users",
+  USER = "/user",
+}
